a8a77cb4734d39656c146731f85f533b07515212
Aaron Blankstein <aaron@blockstack.com>
2021-09-14 15:30:02 -0500

    feat: add handlers for the special functions, but also skip them, because they are currently conflated with MARF runtime

--- proposal/make_cost_functions.py
+++ proposal/make_cost_functions.py
@@ -2,7 +2,14 @@
 
 import csv
 
-to_skip = []
+to_skip = ['cost_at_block', 'cost_create_ft', 'cost_block_info', 'cost_stx_balance',
+           'cost_stx_transfer', 'cost_ft_mint', 'cost_ft_transfer', 'cost_ft_balance',
+           'cost_ft_get_supply', 'cost_ft_burn', 'poison_microblock', 'cost_analysis_storage',
+           'cost_analysis_use_trait_entry', 'cost_analysis_get_function_entry',
+           'cost_load_contract', 'cost_create_map', 'cost_create_var', 'cost_create_nft',
+           'cost_fetch_entry', 'cost_set_entry', 'cost_fetch_var', 'cost_set_var',
+           'cost_contract_storage', 'cost_nft_mint', 'cost_nft_transfer', 'cost_nft_owner',
+           'cost_nft_burn']
 
 function_name_to_type = {}
 
@@ -12,6 +19,282 @@ SCALE_NUMERATOR = 5e9
 SCALE_DENOMINATOR = 3e10 * 100
 SCALE = SCALE_NUMERATOR / SCALE_DENOMINATOR
 
+# special functions that use linear scaling on one constant factors
+special_functions_lin_scale_1 = {
+    "cost_at_block": """
+(define-read-only (cost_at_block (n uint))
+    {{
+        runtime: u{},
+        write_length: u0,
+        write_count: u0,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+    "cost_create_ft": """
+(define-read-only (cost_create_ft (n uint))
+    {{
+        runtime: u{},
+        write_length: u1,
+        write_count: u2,
+        read_count: u0,
+        read_length: u0
+    }})
+""",
+    "cost_block_info": """
+(define-read-only (cost_block_info (n uint))
+    {{
+        runtime: u{},
+        write_length: u0,
+        write_count: u0,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+    "cost_stx_balance": """
+(define-read-only (cost_stx_balance (n uint))
+    {{
+        runtime: u{},
+        write_length: u0,
+        write_count: u0,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+    "cost_stx_transfer": """
+(define-read-only (cost_stx_transfer (n uint))
+    {{
+        runtime: u{},
+        write_length: u1,
+        write_count: u1,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+    "cost_ft_mint": """
+(define-read-only (cost_ft_mint (n uint))
+    {{
+        runtime: u{},
+        write_length: u1,
+        write_count: u2,
+        read_count: u2,
+        read_length: u1
+    }})
+""",
+    "cost_ft_transfer": """
+(define-read-only (cost_ft_transfer (n uint))
+    {{
+        runtime: u{},
+        write_length: u1,
+        write_count: u2,
+        read_count: u2,
+        read_length: u1
+    }})
+""",
+    "cost_ft_balance": """
+(define-read-only (cost_ft_balance (n uint))
+    {{
+        runtime: u{},
+        write_length: u0,
+        write_count: u0,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+    "cost_ft_get_supply": """
+(define-read-only (cost_ft_get_supply (n uint))
+    {{
+        runtime: u{},
+        write_length: u0,
+        write_count: u0,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+    "cost_ft_burn": """
+(define-read-only (cost_ft_burn (n uint))
+    {{
+        runtime: u{},
+        write_length: u1,
+        write_count: u2,
+        read_count: u2,
+        read_length: u1
+    }})
+""",
+    "poison_microblock": """
+(define-read-only (poison_microblock (n uint))
+    {{
+        runtime: u{},
+        write_length: u1,
+        write_count: u1,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+}
+# special functions that use linear scaling on two constant factors
+special_functions_lin_scale_2 = {
+    "cost_analysis_storage": """(define-read-only (cost_analysis_storage (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: (linear n u1 u1),
+        write_count: u1,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+    "cost_analysis_use_trait_entry": """(define-read-only (cost_analysis_use_trait_entry (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: (linear n u1 u1),
+        write_count: u0,
+        read_count: u1,
+        read_length: (linear n u1 u1)
+    }})
+""",
+    "cost_analysis_get_function_entry": """
+(define-read-only (cost_analysis_get_function_entry (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: u0,
+        write_count: u0,
+        read_count: u1,
+        read_length: (linear n u1 u1)
+    }})
+""",
+    "cost_load_contract": """
+(define-read-only (cost_load_contract (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: u0,
+        write_count: u0,
+        ;; set to 3 because of the associated metadata loads
+        read_count: u3,
+        read_length: (linear n u1 u1)
+    }})
+""",
+    "cost_create_map": """
+(define-read-only (cost_create_map (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: (linear n u1 u1),
+        write_count: u1,
+        read_count: u0,
+        read_length: u0
+    }})
+""",
+    "cost_create_var": """
+(define-read-only (cost_create_var (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: (linear n u1 u1),
+        write_count: u2,
+        read_count: u0,
+        read_length: u0
+    }})
+""",
+    "cost_create_nft": """
+(define-read-only (cost_create_nft (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: (linear n u1 u1),
+        write_count: u1,
+        read_count: u0,
+        read_length: u0
+    }})
+""",
+    "cost_fetch_entry": """
+(define-read-only (cost_fetch_entry (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: u0,
+        write_count: u0,
+        read_count: u1,
+        read_length: (linear n u1 u1)
+    }})
+""",
+    "cost_set_entry": """
+(define-read-only (cost_set_entry (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: (linear n u1 u1),
+        write_count: u1,
+        read_count: u1,
+        read_length: u0
+    }})
+""",
+    "cost_fetch_var": """
+(define-read-only (cost_fetch_var (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: u0,
+        write_count: u0,
+        read_count: u1,
+        read_length: (linear n u1 u1)
+    }})
+""",
+    "cost_set_var": """
+(define-read-only (cost_set_var (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: (linear n u1 u1),
+        write_count: u1,
+        read_count: u1,
+        read_length: u0
+    }})
+""",
+    "cost_contract_storage": """
+(define-read-only (cost_contract_storage (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: (linear n u1 u1),
+        write_count: u1,
+        read_count: u0,
+        read_length: u0
+    }})
+""",
+    "cost_nft_mint": """
+(define-read-only (cost_nft_mint (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: u1,
+        write_count: u1,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+    "cost_nft_transfer": """
+(define-read-only (cost_nft_transfer (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: u1,
+        write_count: u1,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+    "cost_nft_owner": """
+(define-read-only (cost_nft_owner (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: u0,
+        write_count: u0,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+    "cost_nft_burn": """
+(define-read-only (cost_nft_burn (n uint))
+    {{
+        runtime: (linear n u{} u{}),
+        write_length: u1,
+        write_count: u1,
+        read_count: u1,
+        read_length: u1
+    }})
+""",
+}
+
 def make_clarity_cost_function(function_name, a_const, b_const):
     a_float = float(a_const)
     b_float = float(b_const)
@@ -20,6 +303,9 @@ def make_clarity_cost_function(function_name, a_const, b_const):
     # type of function we have. we do not have any function
     # types currently that require non-linear scaling, but
     # we may eventually
+    if function_name in to_skip:
+        print("SKIP: %s" % function_name)
+        return None
 
     if function_name in function_name_to_type:
         func_type = function_name_to_type[function_name]
@@ -48,8 +334,18 @@ def make_clarity_cost_function(function_name, a_const, b_const):
 """ % (function_name, a_int, b_int)
         else:
             print("ERROR: unknown type %s for %s" % (func_type, function_name))
+            return None
     else:
-        print("SPECIAL CASE: %s" % function_name)
+        if function_name in special_functions_lin_scale_2:
+            a_int = int(a_float * SCALE)
+            b_int = int(b_float * SCALE)
+            return special_functions_lin_scale_2[function_name].format(a_int, b_int)
+        elif function_name in special_functions_lin_scale_1:
+            b_int = int(b_float * SCALE)
+            return special_functions_lin_scale_1[function_name].format(b_int)
+        else:
+            print("Unhandled special case: %s" % function_name)
+            return None
 
 def load_function_name_types(filename):
     with open(filename, 'r') as raw_file:
