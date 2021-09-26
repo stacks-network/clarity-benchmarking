08eafcf05212290aa434aa2072cfc766a263d344
Greg Coppola <greg@hiro.so>
2021-09-26 20:39:08 +0000

    added criterion size

--- analysis/measure_criterion_size.py
+++ analysis/measure_criterion_size.py
@@ -2,10 +2,9 @@
 
 import os
 import json
-import seaborn as sns
 import csv
+import collections
 
-sns.set_theme()
 
 report = {}
 function_name_to_type = {}
@@ -14,22 +13,21 @@ def load_reports():
     paths = [f.path for f in os.scandir('target/criterion/') if f.is_dir()]
     paths.remove('target/criterion/report')
 
+    name_to_sizes = collections.defaultdict(list)
     for path in paths:
-        print('path:', path)
         path_end = path.split('/')[-1].split(' ')
         function_name = path_end[0]
 
         size_paths = [f.path for f in os.scandir(path) if f.is_dir() and not f.path.endswith('report')]
 
         for size_path in size_paths:
-            print('size_path:', size_path)
             size = int(size_path.split('/')[-1])
-            with open(os.path.join(size_path, 'base', 'estimates.json'), 'r') as f:
-                data = json.load(f)
-                if function_name not in report:
-                    report[function_name] = {}
-                report[function_name][size] = data['median']['point_estimate']
+            name_to_sizes[function_name].append(size)
 
+    sorted_names = sorted(name_to_sizes.keys())
+    for name in sorted_names:
+        sizes = name_to_sizes[name]
+        print(name, sorted(sizes))
 def main():
     load_reports()
 
