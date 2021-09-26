7b64338b700ba84cb82fcebeae6d42f635bf80b1
Greg Coppola <greg@hiro.so>
2021-09-26 22:29:44 +0000

    output one graph for each thing in my pr

---
+++ analysis/run_pr20.sh
@@ -0,0 +1,7 @@
+for MODEL_NAME in cost_fetch_entry cost_set_entry cost_fetch_var cost_set_var cost_list_cons cost_hash160 cost_sha256 cost_sha512 cost_sha512t256 cost_keccak256 cost_print
+do
+echo $MODEL_NAME
+
+python3 analysis/cost_estimator.py ${MODEL_NAME}
+
+done
