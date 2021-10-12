c88153fe3a709cc694f64739a298cd90b3dbf9f4
Greg Coppola <greg@hiro.so>
2021-10-12 19:21:41 +0000

    kind of working but takes too long

--- http-profiling/benchmark.js
+++ http-profiling/benchmark.js
@@ -1,10 +1,12 @@
 const axios = require('axios');
 
-const NUM_REQUESTS = 1000;
+// const NUM_REQUESTS = 1000;
+const NUM_REQUESTS = 3;
 
 var results = [];
 for (var i = 0; i < NUM_REQUESTS; ++i) {
-    const result = axios.get('https://stacks-node-api.mainnet.stacks.co/v2/accounts/SP1P72Z3704VMT3DMHPP2CB8TGQWGDBHD3RPR9GZS')
+    const result = axios.get('http://localhost:20443/v2/accounts/SP1P72Z3704VMT3DMHPP2CB8TGQWGDBHD3RPR9GZS')
+    // const result = axios.get('https://stacks-node-api.mainnet.stacks.co/v2/accounts/SP1P72Z3704VMT3DMHPP2CB8TGQWGDBHD3RPR9GZS')
     results.push(result)
 }
 
@@ -15,9 +17,12 @@ console.log({
 var settled = 0;
 while (settled < NUM_REQUESTS) {
     for (const promise of results) {
+        promise.then(result => {
         settled += 1;
         console.log({
-            settled
+            settled,
+            promise,
+        })
         })
     }
 }
