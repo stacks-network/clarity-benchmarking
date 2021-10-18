48b87b7dd07d44c43a350189696789391b8ef771
Greg Coppola <greg@hiro.so>
2021-10-18 20:22:01 +0000

    removed the make data script

--- marf-profiling/mmap-experiments/make_data.sh
+++
@@ -1,2 +0,0 @@
-head -n 1000000 ~/data/no_mmap.log | python3 extract_data.py no_mmap > data/no_mmap.txt
-head -n 1000000 ~/data/with_mmap.log | python3 extract_data.py with_mmap > data/with_mmap.txt
